181893aaf0b36814a2dcfa70e946458f9206937f
Bernardo Silva <bfalmeidasilva@tecnico.ulisboa.pt>
2022-12-06 17:56:13 +0000

    Cleaned up code

--- chase-client.c
+++ chase-client.c
@@ -1,24 +1,22 @@
 #include <stdlib.h>
 #include <ncurses.h>
 
-
 #include "chase.h"
 
 WINDOW * message_win;
-
 
 typedef struct player_position_t{
     int x, y;
     char c;
 } player_position_t;
 
-void new_player (player_position_t * player, char c){
+void new_player (player_position_t *player, char c){
     player->x = WINDOW_SIZE/2;
     player->y = WINDOW_SIZE/2;
     player->c = c;
 }
 
-void draw_player(WINDOW *win, player_position_t * player, int delete){
+void draw_player(WINDOW *win, player_position_t *player, int delete){
     int ch;
     if(delete){
         ch = player->c;
@@ -32,7 +30,7 @@ void draw_player(WINDOW *win, player_position_t * player, int delete){
     wrefresh(win);
 }
 
-void moove_player (player_position_t * player, int direction){
+void move_player (player_position_t * player, int direction){
     if (direction == KEY_UP){
         if (player->y  != 1){
             player->y --;
@@ -43,8 +41,6 @@ void moove_player (player_position_t * player, int direction){
             player->y ++;
         }
     }
-    
-
     if (direction == KEY_LEFT){
         if (player->x  != 1){
             player->x --;
@@ -59,33 +55,32 @@ void moove_player (player_position_t * player, int direction){
 player_position_t p1;
 
 int main(){
-	initscr();		    	/* Start curses mode 		*/
-	cbreak();				/* Line buffering disabled	*/
-    keypad(stdscr, TRUE);   /* We get F1, F2 etc..		*/
-	noecho();			    /* Don't echo() while we do getch */
+    initscr();              /* Start curses mode */
+    cbreak();               /* Line buffering disabled */
+    keypad(stdscr, TRUE);   /* We get F1, F2 etc... */
+    noecho();               /* Don't echo() while we do getch */
 
     /* creates a window and draws a border */
     WINDOW * my_win = newwin(WINDOW_SIZE, WINDOW_SIZE, 0, 0);
     box(my_win, 0 , 0);	
-	wrefresh(my_win);
+    wrefresh(my_win);
     keypad(my_win, true);
+
     /* creates a window and draws a border */
     message_win = newwin(5, WINDOW_SIZE, WINDOW_SIZE, 0);
     box(message_win, 0 , 0);	
-	wrefresh(message_win);
-
+    wrefresh(message_win);
 
     new_player(&p1, 'y');
     draw_player(my_win, &p1, true);
 
     int key = -1;
-    while(key != 27 && key!= 'q'){
+    while(key != 27 && key != 'q'){
         key = wgetch(my_win);		
         if (key == KEY_LEFT || key == KEY_RIGHT || key == KEY_UP || key == KEY_DOWN){
             draw_player(my_win, &p1, false);
-            moove_player (&p1, key);
+            move_player (&p1, key);
             draw_player(my_win, &p1, true);
-
         }
 
         mvwprintw(message_win, 1,1,"%c key pressed", key);
